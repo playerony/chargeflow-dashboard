58e1d193a9f0a6a5456c92def178e25816b7e700
playerony <pawel.wojtasinski.1995@gmail.com>
2024-01-04 12:03:52 +0100

    feat: create switch component

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,13 +1,11 @@
 import { Typography } from "../../components";
 import "./Dashboard.styles.css";
-import { TopBar } from "./parts";
+import { SideBar, TopBar } from "./parts";
 
 export const Dashboard = () => (
   <main className="dashboard">
     <TopBar />
-    <div className="dashboard__sidebar">
-      <Typography>Sidebar</Typography>
-    </div>
+    <SideBar />
     <div className="dashboard__content">
       <Typography>Content</Typography>
     </div>
