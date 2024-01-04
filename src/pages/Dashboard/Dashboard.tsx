93d7fd2a15632746ef8a70120724d7494b1990e0
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2024-01-04 13:42:25 +0100

    feat: create content with title component (#7)

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,13 +1,18 @@
 import { Typography } from "../../components";
 import "./Dashboard.styles.css";
-import { SideBar, TopBar } from "./parts";
+import { ContentWithTitle, SideBar, TopBar } from "./parts";
 
 export const Dashboard = () => (
   <main className="dashboard">
     <TopBar />
     <SideBar />
     <div className="dashboard__content">
-      <Typography>Content</Typography>
+      <ContentWithTitle title="Today">
+        <Typography>Dashboard 1</Typography>
+      </ContentWithTitle>
+      <ContentWithTitle title="Reports overview">
+        <Typography>Dashboard 2</Typography>
+      </ContentWithTitle>
     </div>
   </main>
 );
