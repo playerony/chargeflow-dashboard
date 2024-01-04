e03aa1d9e1688b38472b3c8901718d6a88de66d5
playerony <pawel.wojtasinski.1995@gmail.com>
2024-01-04 11:36:47 +0100

    feat: create topbar

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,9 +1,15 @@
 import { Typography } from "../../components";
-
 import "./Dashboard.styles.css";
+import { TopBar } from "./parts";
 
 export const Dashboard = () => (
   <main className="dashboard">
-    <Typography>Dashboard</Typography>
+    <TopBar />
+    <div className="dashboard__sidebar">
+      <Typography>Sidebar</Typography>
+    </div>
+    <div className="dashboard__content">
+      <Typography>Content</Typography>
+    </div>
   </main>
 );
